d26ae35b0ee5cc13573ea9cf4df7b38ea2a905d6
Victor <victor.r.rogers@gmail.com>
2015-10-06 17:36:20 -0500

    Initial commit

---
+++ source/src/main.cpp
@@ -0,0 +1,16 @@
+/* Main.cpp
+ * Entry point
+*/
+
+#include <SDL2/SDL.h>
+#include "graphics.h"
+
+int main(int argc, char* args[]) {
+  Graphics graphics;
+
+  while (true) {
+
+  }
+
+	return 0;
+}
